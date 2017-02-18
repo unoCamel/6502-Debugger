9e7d35dbe8d645e7b68d0d0d7cfb017180d7afee
FrazerBayley <bayley.frazer@gmail.com>
2017-02-17 18:01:54 -0800

    Added comments to Assembly.java

--- src/Assembly.java
+++ src/Assembly.java
@@ -5,23 +5,51 @@ public class Assembly{
 	private String[] instructions;
 	private HashMap<String, Integer> labels;
 
+    /*@brief Initializes Assembly class, purpose is to hold instruction and 
+    * their corresponding labels.
+	* 
+	* @param String[] ins all instructions, HashMap<String, Integer> ls labels 
+	* as keys and indexes of labels as values.
+	* @return None.
+	*/
 	public Assembly(String[] ins, HashMap<String, Integer> ls){
 		instructions = ins;
 		labels = ls;
 	}
 
+    /*@brief Gets the array of instructions when called. Array is size 
+    * Global.MAX_SIZE/8
+	* 
+	* @param None.
+	* @return String[] instructions
+	*/
 	public String[] getAllInstructions(){
 		return instructions;
 	}
 
+    /*@brief Gets the HashMap of labels and their indexes.
+	* 
+	* @param None.
+	* @return HashMap<String, Integer> labels
+	*/
 	public HashMap<String, Integer> getAllLabels(){
 		return labels;
 	}
 
+    /*@brief Gets a single instruction from array
+	* 
+	* @param int i index of instruction you would like
+	* @return String instructions[i]
+	*/
 	public String getInstruction(int i){
 		return instructions[i];
 	}
-	
+
+    /*@brief Gets a single corresponding index to label passed.
+	* 
+	* @param String key label
+	* @return int index of label
+	*/
 	public int getLabelIndex(String key){
 		return labels.get(key);
 	}
