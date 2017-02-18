be6c2db2496251b8652149474706b7e210b0db91
FrazerBayley <bayley.frazer@gmail.com>
2017-02-17 20:41:46 -0800

    Added some tests to Import.

--- src/Import.java
+++ src/Import.java
@@ -101,4 +101,38 @@ public class Import {
 		Assembly assembly = new Assembly(instructions, labels);
 		return assembly;
 	}
+
+	public static void testImport(){
+		String str0 = ""; 
+		//no input
+		String str1 = "WEEKDAY:\nCPX #3          ; Year starts in March to bypass\nBCS MARCH       ; leap year problem\nDEY             ; If Jan or Feb, decrement year\nMARCH:    EOR #$7F        ; Invert A so carry works right\nCPY #200        ; Carry will be 1 if 22nd century\nADC MTAB-1,X    ; A is now day+month offset\nSTA TMP\nTYA             ; Get the year\nJSR MOD7        ; Do a modulo to prevent overflow\nSBC TMP         ; Combine with day+month\nSTA TMP\nTYA             ; Get the year again\nLSR             ; Divide it by 4\nLSR\nCLC             ; Add it to y+m+d and fall through\nADC TMP\nMOD7:     ADC #7          ; Returns (A+3) modulo 7\nBCC MOD7        ; for A in 0..255\nRTS\nMTAB:     DB 1,5,6,3,1,5,3,0,4,2,6,4   	; Month offsets"; 
+		//perfect input
+		String str2 = "\n\n\n\nWEEKDAY:\nCPX #3          ; Year starts in March to bypass\nBCS MARCH       ; leap year problem\n\n\n\nDEY             ; If Jan or Feb, decrement year\nMARCH:    EOR #$7F        ; Invert A so carry works right\nCPY #200        ; Carry will be 1 if 22nd century\n\n\n\nADC MTAB-1,X    ; A is now day+month offset\nSTA TMP\nTYA             ; Get the year\nJSR MOD7        ; Do a modulo to prevent overflow\nSBC TMP         ; Combine with day+month\nSTA TMP\nTYA             ; Get the year again\nLSR             ; Divide it by 4\nLSR\nCLC             ; Add it to y+m+d and fall through\nADC TMP\nMOD7:     ADC #7          ; Returns (A+3) modulo 7\nBCC MOD7        ; for A in 0..255\nRTS\nMTAB:     DB 1,5,6,3,1,5,3,0,4,2,6,4   	; Month offsets\n\n\n"; 
+		//extra / unecessary new lines everywhere
+
+		String[] myArray = {str0, str1, str2};
+
+		for (int i = 0; i < myArray.length; i++){
+			System.out.println("\n\n\nSTRING TO BE PARSED:\n" + myArray[i]);
+			Assembly assembly = importInstructions(myArray[i]);
+			System.out.println("INSTRUCTIONS:\n" + Arrays.toString(assembly.getAllInstructions()));
+			System.out.println("\nLABELS AND INDEXES:\n");
+			(assembly.getAllLabels()).forEach((k,v)-> System.out.println(k+", "+v));
+		}
+
+		System.out.println("\n\n\n\nDONE WITH TEST.");
+	}
+
+	public static void main(String[] args){
+		testImport();
+	}
+
+
+
+
+
+
+
+
+
 }
