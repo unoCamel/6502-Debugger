2eaa3b61038620218c657e6c3843e19b0378d481
aziz94shh <aalheid4@uoregon.edu>
2017-02-18 14:12:42 -0800

    upload to git

--- src/Assembly.java
+++ src/Assembly.java
@@ -5,10 +5,10 @@ public class Assembly{
 	private String[] instructions;
 	private HashMap<String, Integer> labels;
 
-    /*@brief Initializes Assembly class, purpose is to hold instruction and 
+    /*@brief Initializes Assembly class, purpose is to hold instruction and
     * their corresponding labels.
-	* 
-	* @param String[] ins all instructions, HashMap<String, Integer> ls labels 
+	*
+	* @param String[] ins all instructions, HashMap<String, Integer> ls labels
 	* as keys and indexes of labels as values.
 	* @return None.
 	*/
@@ -17,9 +17,9 @@ public class Assembly{
 		labels = ls;
 	}
 
-    /*@brief Gets the array of instructions when called. Array is size 
+    /*@brief Gets the array of instructions when called. Array is size
     * Global.MAX_SIZE/8
-	* 
+	*
 	* @param None.
 	* @return String[] instructions
 	*/
@@ -28,7 +28,7 @@ public class Assembly{
 	}
 
     /*@brief Gets the HashMap of labels and their indexes.
-	* 
+	*
 	* @param None.
 	* @return HashMap<String, Integer> labels
 	*/
@@ -37,7 +37,7 @@ public class Assembly{
 	}
 
     /*@brief Gets a single instruction from array
-	* 
+	*
 	* @param int i index of instruction you would like
 	* @return String instructions[i]
 	*/
@@ -46,12 +46,156 @@ public class Assembly{
 	}
 
     /*@brief Gets a single corresponding index to label passed.
-	* 
+	*
 	* @param String key label
 	* @return int index of label
 	*/
 	public int getLabelIndex(String key){
 		return labels.get(key);
 	}
+
+	/*@brief Initializes Assembly class, purpose is to hold instruction and
+	* their corresponding labels.
+*
+* @param String[] ins all instructions, HashMap<String, Integer> ls labels
+* as keys and indexes of labels as values.
+* @return None.
+*/
+public assemble(){
+		binaryInstructions = new int[instructions.length];
+
+		for (String instruction  : instructions) {
+			if (instruction.equals("LABEL")) continue;
+			checkMode(instruction);
+			switch (instruction.substring(0, 3)) {
+				case "ADC":	break;
+				case "AND":	break;
+				case "ASL":	break;
+				case "BCC":	break;
+				case "BCS":	break;
+				case "BEQ":	break;
+				case "BIT":	break;
+				case "BMI":	break;
+				case "BNE":	break;
+				case "BPL":	break;
+				case "BRK":	break;
+				case "BVC":	break;
+				case "BVS":	break;
+				case "CLC":	break;
+				case "CLD":	break;
+				case "CLI":	break;
+				case "CLV":	break;
+				case "CMP":	break;
+				case "CPX":	break;
+				case "CPY":	break;
+				case "DEC":	break;
+				case "DEX":	break;
+				case "DEY":	break;
+				case "EOR":	break;
+				case "INC":	break;
+				case "INX":	break;
+				case "INY":	break;
+				case "JMP":	break;
+				case "JSR":	break;
+				case "LDA":	break;
+				case "LDX":	break;
+				case "LDY":	break;
+				case "LSR":	break;
+				case "NOP":	break;
+				case "ORA":	break;
+				case "PHA":	break;
+				case "PHP":	break;
+				case "PLA":	break;
+				case "PLP":	break;
+				case "ROL":	break;
+				case "ROR":	break;
+				case "RTI":	break;
+				case "RTS":	break;
+				case "SBC":	break;
+				case "SEC":	break;
+				case "SED":	break;
+				case "SEI":	break;
+				case "STA":	break;
+				case "STX":	break;
+				case "STY":	break;
+				case "TAX":	break;
+				case "TAY":	break;
+				case "TSX":	break;
+				case "TXA":	break;
+				case "TXS":	break;
+				case "TYA":	break;
+				default:	break;
+
+
+			}
+
+		}
+}
+
+	private int checkMode(instruction){
+			int modebit = 0;
+			// (1) Implicit / (2) Accumulator / (3) Immediate / (4) Zero Page / (5) Zero Page,X / (6) Zero Page,Y / (7) Relative
+			// (8) Absolute / (9) Absolute,X / (10) Absolute,Y / (11) Indirect / (12) (Indirect,X) / (13) (Indirect,Y)
+	    if (checkImplicit(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkAccumulator(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkImmediate(instruction)){modebit = modebit | (1 << 2);}
+	    if (checkZeroPage(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkZeroPageX(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkZeroPageY(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkRelative(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkAbsolute(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkAbsoluteX(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkAbsoluteY(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkIndirect(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkIndirectX(instruction)){modebit = modebit | (1 << 0);}
+	    if (checkIndirectY(instruction)){modebit = modebit | (1 << 0);}
+			return modebit;
+	}
+
+	private boolean checkImmediate(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkImplicit(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkAccumulator(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkZeroPage(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkZeroPageX(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkZeroPageY(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+	private boolean checkRelative(String inst){
+		String pattern = "#\$[0-9a-f]{1,2}";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(line);
+		return m.find();
+	}
+
+
+
 
 }
