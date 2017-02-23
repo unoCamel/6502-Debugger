9ee754cf70ea442fddab14a064f0d61f57db12b0
Haley Whitman <hwhitman@uoregon.edu>
2017-02-22 22:07:48 -0800

    Fixed large register bug
    setting and resetting was incorrect.

--- src/Registers_UnitTest.java
+++ src/Registers_UnitTest.java
@@ -8,11 +8,35 @@ public class Registers_UnitTest {
     public static void testReadWrite(){
         Registers.write8(Global.$A, 0x10);
         Assert.assertEquals(0x10, Registers.read8(Global.$A));
+        Registers.write16(Global.$PC, 0xFFFF);
+        Assert.assertEquals(0xFFFF, Registers.read16(Global.$PC));
+        Registers.write16(Global.$SP, 0x0);
+        Assert.assertEquals(0x0, Registers.read16(Global.$SP));
+    }
+
+    public static void testPFlags(){
+        System.out.println("value: " + Integer.toBinaryString(Registers.read8(Global.$P)));
+        Assert.assertTrue(Registers.isCarry() == false);
+        Assert.assertTrue(Registers.isNegative() == false);
+        Assert.assertTrue(Registers.isZero() == false);
+        Registers.setCarry();
+        Registers.setNegative();
+        System.out.println("value: " + Integer.toBinaryString(Registers.read8(Global.$P)));
+        Assert.assertTrue(Registers.isCarry() == true);
+        Assert.assertTrue(Registers.isZero() == false);
+        Assert.assertTrue(Registers.isNegative() == true);
+        Registers.resetNegative();
+        System.out.println("value: " + Integer.toBinaryString(Registers.read8(Global.$P)));
+        Assert.assertTrue(Registers.isCarry() == true);
+        Assert.assertTrue(Registers.isNegative() == false);
+
+
     }
 
     public static void runRegisterSuite(){
             System.out.println("Starting Register Tests");
             testReadWrite();
+            testPFlags();
     }
 
 }
